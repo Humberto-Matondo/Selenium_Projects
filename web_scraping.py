d9d9c4a3c246b62eb8642330a377e3733bc5b8f1
Humberto-Matondo <humbertomat99@gmail.com>
2023-11-21 11:46:12 +0100

    web scraping vol 2

--- web_scraping.py
+++ web_scraping.py
@@ -9,4 +9,14 @@ response = requests.get(url)
 raw_html = response.text 
 parsed_html = BeautifulSoup(raw_html, 'html.parser') 
 
-print(parsed_html.title) 
+# print(parsed_html.title) 
+
+selector_copiado = parsed_html.select_one('#top3 > div > div > article:nth-child(3) > h3') 
+
+if selector_copiado is not None:
+    tudo_da_tag_article = selector_copiado.parent 
+    print(tudo_da_tag_article)
+
+for p in tudo_da_tag_article.select('p'): 
+    print(p) 
+    print(p.text) 
